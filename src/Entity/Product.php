ea0a87b435e05d9cc4abfe1bc2d05258b27cf935
luanapatricia <luanapatricia@alu.ufc.br>
2020-11-12 22:01:00 +0100

    classe projet et test

---
+++ src/Entity/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+class Product
+{
+    const FOOD_PRODUCT = 'food';
+
+    private $name;
+
+    private $type;
+
+    private $price;
+
+    public function __construct($name, $type, $price)
+    {
+        $this->name = $name;
+        $this->type = $type;
+        $this->price = $price;
+    }
+
+    public function computeTVA()
+    {
+        if (self::FOOD_PRODUCT == $this->type) {
+            return $this->price * 0.055;
+        }
+
+        if ($this->price < 0) {
+            throw new \LogicException('The TVA cannot be negative.');
+        }
+        
+        return $this->price * 0.196;
+        
+    }
+}
---
+++ tests/Entity/ProductTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Entity;
+
+use App\Entity\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testcomputeTVAFoodProduct()
+    {
+        $product = new Product('Un produit', Product::FOOD_PRODUCT, 10);
+        $this->assertSame(0.5, $product->computeTVA());
+    }
+
+    // public function testcomputeTVAFoodProduct($price, $expectedTva)
+    // {
+    //     $product = new Product('Un produit', Product::FOOD_PRODUCT, $price);
+
+    //     $this->assertSame($expectedTva, $product->computeTVA());
+    // }
+
+    // public function pricesForFoodProduct()
+    // {
+    //     return [
+    //         [0, 0.0],
+    //         [20, 1.1],
+    //         [100, 5.5]
+    //     ];
+    // }
+
+    public function testcomputeTVAOtherProduct()
+    {
+        $product = new Product('Un produit', 'Autre produit', 10);
+        $this->assertSame(1.96, $product->computeTVA());
+    }
+
+    public function testcomputeTVANegativePrice()
+    {
+        $product = new Product('Un produit', 'Autre produit', -10);
+        $this->expectException('LogicException');
+        $product->computeTVA();
+    }
+}
